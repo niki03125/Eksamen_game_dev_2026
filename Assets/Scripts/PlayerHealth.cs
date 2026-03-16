d4bbccdde0486b4972b50de0ed49521a8fda67a4
Nico538e <nicolai-strand@hotmail.com>
2026-03-16 10:44:27 +0100

    health på player
    player health ikke testet

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    public float currentHealth;
+    
+    public Slider healthBar;
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth < 0)
+            currentHealth = 0;
+
+        healthBar.value = currentHealth;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player has died!");
+        // Implement death behavior (e.g., respawn, game over screen)
+        
+    }
+    
+}
